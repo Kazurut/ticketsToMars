866faf5cbcd90ba6c764f879d4b6bf04cef8a850
Egor Sevostyanov <e.sevastyanov@pos-credit.ru>
2023-01-27 17:45:53 +0300

    add tickets list

---
+++ index.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	const (
+		distance      = 62100000
+		priceConst    = 20
+		secondsPerDay = 86400
+	)
+	var companies = [3]string{"Virgin Galactic", "SpaceX", "Space Adventures"}
+	var tripTypeName = [2]string{"One-way", "Round-trip"}
+
+	fmt.Printf(
+		"%-16v %4v %2v %4v\n====================================\n",
+		"Spaceline", "Days", "Trip type",
+	)
+
+	for count := 0; count < 10; count++ {
+		station := companies[rand.Intn(2)]
+		speed := rand.Intn(15) + 16
+		tripType := rand.Intn(2) + 1
+		price := (priceConst + speed) * tripType
+		time := distance / speed / secondsPerDay // days
+
+		fmt.Printf("%-16v %3v %-10v $%3v\n", station, time, tripTypeName[tripType-1], price)
+	}
+}
